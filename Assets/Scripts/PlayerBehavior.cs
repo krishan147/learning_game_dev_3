f747245a13d09adf5cb2f772c62052dd93ce2c73
krishan vadher <krishan.vadher@gmail.com>
2020-01-03 21:19:10 +0000

    first commit

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    // 1
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 75f;
+
+    //2
+    private float vInput;
+    private float hInput;
+
+    private void Update()
+    {
+        //3
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+
+        //4
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+
+        //5
+        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+
+        //6
+        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+    }
+}
