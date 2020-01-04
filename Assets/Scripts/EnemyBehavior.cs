fb04b5f16a1436f14e70f2829d774c3f1e870971
krishan <krishan.vadher@gmail.com>
2020-01-04 15:48:59 +0000

    added enemy

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player detecterd - attack!");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player out of range, resumse patrol");
+        }
+    }
+
+}
