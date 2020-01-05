346bf07d43ef90e5886287803f659d20c0ff8faa
krishan <krishan.vadher@gmail.com>
2020-01-05 21:40:11 +0000

    added enemy ptrl

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public Transform patrolRoute;
+
+    public List<Transform> locations;
+
+    void Start()
+    {
+        InitializePatrolRoute();
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach(Transform child in patrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+
     void OnTriggerEnter(Collider other)
     {
         if(other.name == "Player")
